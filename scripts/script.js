a577ab757e1977adcc67cacd8c51f37bebe4aa6d
Lidiiaa <lidakulagina@gmail.com>
2022-10-01 13:36:07 +0100

    2nd commit

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,12 @@
 $( document ).ready(function() {
     $('table tr td:nth-child(odd)').addClass('comment');
     $('table tr td:nth-child(even)').addClass('number');
+    $( "#addDette" ).click(function() {
+        $('<tr><td class="comment"></td></tr><tr><td class="number"></td></tr>').insertBefore($('.tableDette tbody tr.totals'));
+        $('inputsDette').submit(function(e){
+            
+        });
+    });
+   
+
 });
