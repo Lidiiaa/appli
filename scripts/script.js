342b03b29940939344788699bf6811a32c1157a8
Lidiiaa <lidakulagina@gmail.com>
2022-10-14 19:59:17 +0100

    hey

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,11 @@ $( document ).ready(function() {
     $('table tr td:nth-child(even)').addClass('number');
     $( "#addDette" ).click(function() {
         $('<tr><td class="comment"></td></tr><tr><td class="number"></td></tr>').insertBefore($('.tableDette tbody tr.totals'));
-        $('inputsDette').submit(function(e){
-            
+        $('.inputsDette').submit(function(e){
+
         });
     });
-   
+   $('h1').text() = "Meow";
 
 });
+
